878353115eb8bf9674518ee24d4af1ffc1bb8e51
Christopher Scott <cmdruid@aptget.website>
2021-02-14 23:12:43 -0800

    Added some files

---
+++ src/store.js
@@ -0,0 +1,19 @@
+/** store.js
+ * Configures local database storage.
+ * */
+
+import { LocalStorage } from "node-localstorage";
+
+const store = new LocalStorage("./data");
+
+try {
+  store.setItem("test", "success");
+  if (store.getItem("test") !== "success") {
+    throw Error("Datastore failed to load!");
+  }
+  console.log("Datastore loaded!");
+} catch (e) {
+  console.error(e);
+}
+
+export default store;
